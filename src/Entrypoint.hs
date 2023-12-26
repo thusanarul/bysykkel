6394b1c078e152203f5a8517708bf944babe3fac
Thusan Arul <thusan.arul@iterate.no>
2023-12-26 23:01:47 +0100

    Creates server with servant

--- src/Entrypoint.hs
+++ src/Entrypoint.hs
@@ -1,13 +1,19 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
 {-# HLINT ignore "Use newtype instead of data" #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 module Entrypoint (main) where
 
 import Data.Aeson (FromJSON (parseJSON), Value (Object), (.:))
 import Data.Aeson.Types (typeMismatch)
 import qualified Data.ByteString.Char8 as S8
+import Data.Maybe (isNothing)
+import GHC.Generics
 import Network.HTTP.Simple (getResponseBody, httpJSON)
+import Network.Wai.Handler.Warp
+import Servant
 import StationInformation (StationInformation)
 import StationStatus (StationStatus)
 
@@ -37,12 +43,34 @@ instance (FromJSON d) => FromJSON (Model d) where
     pure $ Model {..}
   parseJSON invalid = typeMismatch "Model" invalid
 
+-- /available_stations?lat={lat}&lon={lon}&count={count} -- lat/lon is non-optional
+type BysykkelAPI =
+  "available_stations"
+    :> QueryParam "lat" Float
+    :> QueryParam "lon" Float
+    :> QueryParam "count" Int
+    :> Get '[PlainText] String
+    :<|> "health"
+      :> Get '[PlainText] String
+
+handleAvailableStations :: Maybe Float -> Maybe Float -> Maybe Int -> Handler String
+handleAvailableStations lat lon count =
+  if isNothing lat || isNothing lon
+    then throwError err400 {errBody = "Missing lat and/or lon query parameters"}
+    else return "HEI"
+
+handleHealth :: Handler String
+handleHealth = return "OK"
+
+server :: Server BysykkelAPI
+server = handleAvailableStations :<|> handleHealth
+
 main :: IO ()
 main = do
   putStrLn "Bysykkel API!"
   response <- httpJSON "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json"
   let station_information = (getResponseBody response :: Model StationInformation)
-  print station_information
   response <- httpJSON "https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json"
   let station_status = (getResponseBody response :: Model StationStatus)
-  print station_status
+  -- this does not have to be one-liner, but it's rad yeah
+  run 8000 $ serve (Proxy :: Proxy BysykkelAPI) server
